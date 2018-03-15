311b93f7be0aad47fd37813d2c9402ac86499c21
KD <kate_lani@wp.pl>
2018-03-15 11:48:19 +0100

    replace poltergeist with selenium chromedriver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,17 +1,13 @@
 require 'capybara/rspec'
 require 'capybara/rails'
-require 'capybara/poltergeist'
+require 'selenium-webdriver'
 
 RSpec.configure do |config|
-  Capybara.javascript_driver = :poltergeist
-
-  Capybara.register_driver(:poltergeist) do |app|
-    Capybara::Poltergeist::Driver.new app, timeout: 90
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new app,
+      browser: :chrome,
+      options: Selenium::WebDriver::Chrome::Options.new(args: %w[disable-popup-blocking headless disable-gpu window-size=1920,1080])
   end
 
-  config.before(:each, :js) do
-    if Capybara.javascript_driver == :selenium
-      page.driver.browser.manage.window.maximize
-    end
-  end
+  Capybara.javascript_driver = :chrome
 end
