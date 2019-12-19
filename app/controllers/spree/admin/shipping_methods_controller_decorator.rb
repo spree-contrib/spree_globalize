159c0055de52d79d412d090659d3fb4fb9d33e13
PSzyszkaSpark <pawel.szyszka@sparksolutions.co>
2019-12-19 09:47:29 +0100

    Corrects logic in shipping_methods_controller_decorator.rb

--- app/controllers/spree/admin/shipping_methods_controller_decorator.rb
+++ app/controllers/spree/admin/shipping_methods_controller_decorator.rb
@@ -1,20 +1,15 @@
 module Spree
   module Admin
     module ShippingMethodsControllerDecorator
-      def self.prepended(base)
-        base.alias_method :old_set_shipping_category, :set_shipping_category
-        base.alias_method :old_set_zones, :set_zones
-      end
-
       private
       # We have to override those filters since they break proper PATCH behaviour.
 
       def set_shipping_category
-        old_set_shipping_category unless params['shipping_method'][:translations_attributes]
+        super unless params['shipping_method'][:translations_attributes]
       end
 
       def set_zones
-        old_set_zones unless params['shipping_method'][:translations_attributes]
+        super unless params['shipping_method'][:translations_attributes]
       end
     end
   end
