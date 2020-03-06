66ff82888406786b479ba15dfbe18fcd06e94ecc
Gary Lai <garylai1990@gmail.com>
2020-03-06 14:48:40 -0800

    update style

--- app/overrides/spree/admin/shared/_product_tabs/add_translations.rb
+++ app/overrides/spree/admin/shared/_product_tabs/add_translations.rb
@@ -3,8 +3,8 @@ Deface::Override.new(
   name:          'product_tabs_translation',
   insert_bottom: "[data-hook='admin_product_tabs']",
   text:          <<-HTML
-                  <li class="<%= 'active' if current == 'Translations' %> nav-link">
-                    <%= link_to_with_icon 'translate', Spree.t(:'i18n.translations'), spree.admin_translations_url('products', @product.slug), title: Spree.t(:'i18n.translations') %>
+                  <li>
+                    <%= link_to_with_icon 'translate', Spree.t(:'i18n.translations'), spree.admin_translations_url('products', @product.slug), title: Spree.t(:'i18n.translations'), class: "\#\{'active' if current == 'Translations'\} nav-link" %>
                   </li>
                 HTML
 )
