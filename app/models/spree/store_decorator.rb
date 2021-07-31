ecfb8ce34fd4be1e4ada9280c71dd9c08a8fcf2d
dimidev <dimidev@olympusone.com>
2021-08-01 02:50:37 +0300

    add description, address fields to store decorator

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,7 +1,7 @@
 module Spree::StoreDecorator
   def self.prepended(base)
     if ::ApplicationRecord.connected? && ::ApplicationRecord.connection.table_exists?(:spree_store_translations)
-      base.translates :name, :meta_description, :meta_keywords, :seo_title, fallbacks_for_empty_translations: true
+      base.translates :name, :meta_description, :meta_keywords, :seo_title, :description, :address, fallbacks_for_empty_translations: true
     end
   end
 
