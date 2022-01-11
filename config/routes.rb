ef3850b0463248843bc0d60bd59c682e2b49190d
Juan Carlo Amican <juancarloamican@gmail.com>
2022-01-11 11:26:52 +0800

    Fix: Use Spree Admin path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.add_routes do
-  namespace :admin do
+  namespace :admin, path: Spree.admin_path do
     get '/:resource/:resource_id/translations' => 'translations#index', as: :translations
     patch '/option_values/:id' => 'option_values#update', as: :option_type_option_value
     patch 'product/:id/product_properties/:id' => "product_properties#translate", as: :translate_product_property
